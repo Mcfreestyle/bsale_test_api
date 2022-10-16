5a291eede5586ee96b333f45cddb3b7b58eca4e9
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-10-16 01:32:47 -0500

    chore: now only create flask app

--- app.py
+++ app.py
@@ -10,6 +10,3 @@ app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-
-if __name__ == '__main__':
-  app.run(debug=True)
