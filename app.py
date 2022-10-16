c3fe8b7a0ee96243262e0da6226ba0cd87628396
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-10-16 17:12:50 -0500

    feat: use of swagger to document API

--- app.py
+++ app.py
@@ -5,8 +5,12 @@
 from flask import Flask
 from views import app_views
 from flask_cors import CORS
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['SWAGGER'] = { 'title': 'Bsale API' }
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+Swagger(app)
