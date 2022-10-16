7b1571720ce8edb009d16ff59757058a9fb3f0c0
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-10-15 23:01:54 -0500

    feat: functions views to products routes

---
+++ views/products.py
@@ -0,0 +1,43 @@
+"""
+  This module supplies these functions views:
+    `get_products`,
+    `get_products_by_category`
+"""
+
+from views import app_views
+from flask import make_response, jsonify, abort, request
+from engine.db_storage import DBStorage
+
+@app_views.route('/products', methods=['GET'])
+async def get_products():
+  """
+    Get all products
+  """
+  storage = DBStorage()
+
+  search_var = request.args.get('search')
+  if search_var:
+    products = await storage.find('product', search_var)
+  else:
+    products = await storage.all('product')
+  return make_response(jsonify(products), 200)
+
+@app_views.route('/categories/<int:category_id>/products', methods=['GET'])
+async def get_products_by_category(category_id):
+  """
+    Get all products of a category
+      Args:
+        category_id(int): category id
+  """
+  storage = DBStorage()
+  category = await storage.get('category', category_id)
+  if not category:
+    abort(404)
+
+  category_products = []
+  products = await storage.all('product')
+  for product in products:
+    if product['category'] == category['id']:
+      category_products.append(product)
+  
+  return make_response(jsonify(category_products), 200)
